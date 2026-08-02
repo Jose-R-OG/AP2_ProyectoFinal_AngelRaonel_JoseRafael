478a95bb8703bf3e2ce29f5735a23db104b6d888
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-08-02 17:46:40 -0400

    Creacion de ClienteMapper.kt

---
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/data/mapper/ClienteMapper.kt
@@ -0,0 +1,28 @@
+package com.example.ap2_proyectofinal_angelraonel_joserafael.data.Cliente.mapper
+
+import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Cliente.local.ClienteEntity
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.model.Cliente
+
+fun ClienteEntity.toDomain(): Cliente {
+    return Cliente(
+        id = id,
+        fullName = fullName,
+        dni = dni,
+        phone = phone,
+        address = address,
+        dniFrontPhotoPath = dniFrontPhotoPath,
+        isActive = isActive
+    )
+}
+
+fun Cliente.toEntity(): ClienteEntity {
+    return ClienteEntity(
+        id = id,
+        fullName = fullName,
+        dni = dni,
+        phone = phone,
+        address = address,
+        dniFrontPhotoPath = dniFrontPhotoPath,
+        isActive = isActive
+    )
+}
