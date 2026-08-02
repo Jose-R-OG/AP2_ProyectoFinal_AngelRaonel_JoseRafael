52c0aaafc9d7dcd721513b88f9c2de9f81ff7d31
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-08-02 17:48:19 -0400

    Agrengado dependecias al module

--- app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/di/RepositoryModule.kt
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.example.ap2_proyectofinal_angelraonel_joserafael.di
 
 import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Auth.repository.AuthRepositoryImpl
+import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Cliente.repository.ClienteRepositoryImpl
 import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.repository.AuthRepository
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.repository.ClienteRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,4 +19,10 @@ abstract class RepositoryModule {
     abstract fun bindAuthRepository(
         authRepositoryImpl: AuthRepositoryImpl
     ): AuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindClienteRepository(
+        clienteRepositoryImpl: ClienteRepositoryImpl
+    ): ClienteRepository
 }
