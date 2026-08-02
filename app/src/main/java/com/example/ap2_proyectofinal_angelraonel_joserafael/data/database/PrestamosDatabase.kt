1a03a28a9258ca45968630f5688f5572f5c1325c
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-08-02 18:40:08 -0400

    Agregando la tabla de tarifas

--- app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/data/database/PrestamosDatabase.kt
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/data/database/PrestamosDatabase.kt
@@ -2,17 +2,28 @@ package com.example.ap2_proyectofinal_angelraonel_joserafael.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Auth.local.UserDao
 import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Auth.local.UserEntity
 import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Cliente.local.ClienteDao
 import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Cliente.local.ClienteEntity
+import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Tarifario.local.ConfigDao
+import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Tarifario.local.ConfigEntity
 
 @Database(
-    entities = [UserEntity::class, ClienteEntity::class],
-    version = 2,
+    entities = [
+        UserEntity::class,
+        ClienteEntity::class,
+        ConfigEntity::class
+    ],
+    version = 1,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class PrestamosDatabase : RoomDatabase() {
+
     abstract fun userDao(): UserDao
     abstract fun clienteDao(): ClienteDao
+    abstract fun configDao(): ConfigDao
+
 }
