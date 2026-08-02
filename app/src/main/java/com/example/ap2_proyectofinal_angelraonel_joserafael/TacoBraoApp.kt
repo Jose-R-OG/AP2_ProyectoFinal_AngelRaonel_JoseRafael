6bf90a17339e8c94a7fca9b45ce170de2a779d8a
angel <angel_guerrero1@ucne.edu.do>
2026-08-01 22:41:20 -0400

    Agrega la clase TacoBraoApp con configuración para Hilt

---
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/TacoBraoApp.kt
@@ -0,0 +1,7 @@
+package com.example.ap2_proyectofinal_angelraonel_joserafael
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TacoBraoApp: Application()
