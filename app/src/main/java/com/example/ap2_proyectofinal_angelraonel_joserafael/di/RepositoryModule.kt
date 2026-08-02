7c3b1ec73be274ca58c38b10637d4f34cfeedb29
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-08-02 18:39:25 -0400

    Modificacion de los modules para agregar dependencias de tarifario

--- app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/di/RepositoryModule.kt
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/di/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.example.ap2_proyectofinal_angelraonel_joserafael.di
 
 import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Auth.repository.AuthRepositoryImpl
 import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Cliente.repository.ClienteRepositoryImpl
+import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Tarifario.repository.TarifarioRepositoryImpl
 import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.repository.AuthRepository
 import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.repository.ClienteRepository
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.repository.TarifarioRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -25,4 +27,10 @@ abstract class RepositoryModule {
     abstract fun bindClienteRepository(
         clienteRepositoryImpl: ClienteRepositoryImpl
     ): ClienteRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindTarifarioRepository(
+        tarifarioRepositoryImpl: TarifarioRepositoryImpl
+    ): TarifarioRepository
 }
