0ad81d3ca63803bcd66acb339fe9c6533dcdf78d
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-08-02 17:47:14 -0400

    Creacion del repositorio y su implementacion para clientes

---
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/domain/repository/ClienteRepository.kt
@@ -0,0 +1,11 @@
+package com.example.ap2_proyectofinal_angelraonel_joserafael.domain.repository
+
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.model.Cliente
+import kotlinx.coroutines.flow.Flow
+
+interface ClienteRepository {
+    fun getActiveClientes(): Flow<List<Cliente>>
+    suspend fun getClienteById(id: Long): Cliente?
+    suspend fun saveCliente(cliente: Cliente)
+    suspend fun softDeleteCliente(id: Long)
+}
