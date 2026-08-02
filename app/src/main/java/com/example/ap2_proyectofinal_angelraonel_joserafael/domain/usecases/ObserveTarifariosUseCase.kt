0f2ad867aba4e6436dbbc0b9a555c5c2545716db
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-08-02 18:38:48 -0400

    Creacion de casos de usos para el tarifario

---
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/domain/usecases/ObserveTarifariosUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.ap2_proyectofinal_angelraonel_joserafael.domain.usecase
+
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.model.Tarifario
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.repository.TarifarioRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class ObserveTarifariosUseCase @Inject constructor(
+    private val repository: TarifarioRepository
+) {
+    operator fun invoke(): Flow<List<Tarifario>> {
+        return repository.getActiveTarifarios()
+    }
+}
