ec159f4732c9a19845797aafd3fa440f49f9d248
Jose-R-OG <jose_ortega2@ucne.edu.do>
2026-08-02 21:06:23 -0400

    Creacion de PrestamoMapper.kt

---
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/data/mapper/PrestamoMapper.kt
@@ -0,0 +1,44 @@
+package com.example.ap2_proyectofinal_angelraonel_joserafael.data.Prestamo.mapper
+
+import com.example.ap2_proyectofinal_angelraonel_joserafael.data.local.prestamo.CuotaEntity
+import com.example.ap2_proyectofinal_angelraonel_joserafael.data.Prestamo.local.PrestamoEntity
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.model.Cuota
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.model.Prestamo
+
+fun PrestamoEntity.toDomain(): Prestamo {
+    return Prestamo(
+        id = id, clienteId = clienteId, empleadoId = empleadoId, aprobadoPorAdminId = aprobadoPorAdminId,
+        montoSolicitado = montoSolicitado, porcentajeInteres = porcentajeInteres, interesTotal = interesTotal,
+        totalAPagar = totalAPagar, totalPagado = totalPagado, montoCuota = montoCuota,
+        cantidadCuotas = cantidadCuotas, frecuenciaPago = frecuenciaPago, fechaCreacion = fechaCreacion,
+        fechaInicio = fechaInicio, fechaFin = fechaFin, estado = estado, motivoRechazo = motivoRechazo,
+        rutaFotoContratoFirmado = rutaFotoContratoFirmado, contratoFisicoEntregado = contratoFisicoEntregado
+    )
+}
+
+fun Prestamo.toEntity(): PrestamoEntity {
+    return PrestamoEntity(
+        id = id, clienteId = clienteId, empleadoId = empleadoId, aprobadoPorAdminId = aprobadoPorAdminId,
+        montoSolicitado = montoSolicitado, porcentajeInteres = porcentajeInteres, interesTotal = interesTotal,
+        totalAPagar = totalAPagar, totalPagado = totalPagado, montoCuota = montoCuota,
+        cantidadCuotas = cantidadCuotas, frecuenciaPago = frecuenciaPago, fechaCreacion = fechaCreacion,
+        fechaInicio = fechaInicio, fechaFin = fechaFin, estado = estado, motivoRechazo = motivoRechazo,
+        rutaFotoContratoFirmado = rutaFotoContratoFirmado, contratoFisicoEntregado = contratoFisicoEntregado
+    )
+}
+
+fun CuotaEntity.toDomain(): Cuota {
+    return Cuota(
+        id = id, prestamoId = prestamoId, numeroCuota = numeroCuota, fechaVencimiento = fechaVencimiento,
+        fechaPago = fechaPago, montoEsperado = montoEsperado, montoPagado = montoPagado,
+        moraAcumulada = moraAcumulada, estaPagada = estaPagada
+    )
+}
+
+fun Cuota.toEntity(): CuotaEntity {
+    return CuotaEntity(
+        id = id, prestamoId = prestamoId, numeroCuota = numeroCuota, fechaVencimiento = fechaVencimiento,
+        fechaPago = fechaPago, montoEsperado = montoEsperado, montoPagado = montoPagado,
+        moraAcumulada = moraAcumulada, estaPagada = estaPagada
+    )
+}
