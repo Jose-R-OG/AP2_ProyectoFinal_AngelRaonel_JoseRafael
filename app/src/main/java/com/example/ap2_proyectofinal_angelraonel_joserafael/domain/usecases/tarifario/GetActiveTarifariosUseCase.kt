73567e8bedee4a2459353122b1b0623229e5c250
angel <angel_guerrero1@ucne.edu.do>
2026-08-03 00:33:59 -0400

    Agrega pantalla para ajustar tarifas y caso de uso para obtener tarifarios activos

---
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/domain/usecases/tarifario/GetActiveTarifariosUseCase.kt
@@ -0,0 +1,14 @@
+package com.example.ap2_proyectofinal_angelraonel_joserafael.domain.usecases.tarifario
+
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.model.Tarifario
+import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.repository.TarifarioRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetActiveTarifariosUseCase @Inject constructor(
+    private val repository: TarifarioRepository
+) {
+    operator fun invoke(): Flow<List<Tarifario>> {
+        return repository.getActiveTarifarios()
+    }
+}
