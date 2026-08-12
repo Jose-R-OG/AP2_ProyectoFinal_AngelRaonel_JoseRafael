12e98183846f00b2ecd881686c18c35e52a0d69f
angel <angel_guerrero1@ucne.edu.do>
2026-08-12 18:24:50 -0400

    Agrega Icono

--- app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/example/ap2_proyectofinal_angelraonel_joserafael/presentation/login/LoginScreen.kt
@@ -1,7 +1,17 @@
 package com.example.ap2_proyectofinal_angelraonel_joserafael.presentation.login
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -26,7 +36,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.ap2_proyectofinal_angelraonel_joserafael.domain.model.UserRole
-import com.example.ap2_proyectofinal_angelraonel_joserafael.presentation.login.LoginUiState
 
 @Composable
 fun LoginScreen(
@@ -192,7 +201,8 @@ fun LoginScreen(
                             Text(
                                 text = "Iniciar Sesión",
                                 fontSize = 16.sp,
-                                fontWeight = FontWeight.Bold
+                                fontWeight = FontWeight.Bold,
+                                color = Color.White
                             )
                         }
                     }
@@ -203,7 +213,10 @@ fun LoginScreen(
                         modifier = Modifier.fillMaxWidth().height(50.dp),
                         shape = RoundedCornerShape(12.dp)
                     ) {
-                        Text("Continuar con Google", fontWeight = FontWeight.Bold, color = primaryGreen)
+                        Column{
+                            Text("Continuar con Google", fontWeight = FontWeight.Bold, color = primaryGreen)
+                        }
+
                     }
 
 
